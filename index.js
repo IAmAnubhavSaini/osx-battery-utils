145da9fa20bdf610a649302316823ec401a62eed
Anubhav Saini <ansaini@paypal.com>
2016-07-26 23:35:31 +0530

    battery utils are not functions and battery amperage is added.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const osxBattery = require('osx-battery');
 
-const isCharging = () => osxBattery().then(battery => battery.isCharging);
+const isCharging = osxBattery().then(battery => battery.isCharging);
+const getAmperage = osxBattery().then(battery => battery.amperage);
 
 module.exports = {
+    amperage: getAmperage,
     isCharging: isCharging
 };
--- test.js
+++ test.js
@@ -2,5 +2,5 @@ import test from 'ava';
 import batteryUtils from './';
 
 test(async t => {
-    t.is(typeof await batteryUtils.isCharging(), 'boolean');
+    t.is(typeof await batteryUtils.isCharging, 'boolean');
 });
